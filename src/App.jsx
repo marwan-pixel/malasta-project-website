3cba87930755026f3feeb2bc954b179a074d1521
marwan-pixel <marwanhadid6@gmail.com>
2024-05-19 13:15:46 +0700

    Update Progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="w-full mx-auto bg-blue-300 h-20 ">
-        <h1 className="text-3xl font-bold font-inter block p-5">
-          Hello World!
-        </h1>
-      </div>
-    </>
+    <div className="font-roboto">
+      <Navbar />
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,22 @@
 import * as react from "react";
+
+export default function Navbar() {
+    return (
+        <>
+            <div className="w-full mx-auto flex justify-between items-center bg-white text-customgreen h-[5.8rem] navbar-base">
+                <h1 className="text-[1.7rem] font-semibold font-inter block ml-32">
+                Malasta Project
+                </h1>
+                <div className="navbar-link flex text-[1.3rem] font-semibold mr-32 gap-x-7">
+                    <a href="">Home</a>
+                    <a href="">Rental</a>
+                    <a href="">Catalog</a>
+                </div>
+                <div className="flex navbar-socmed">
+                    <p></p>
+                    <p></p>
+                </div>
+            </div>
+        </>
+    );
+}
